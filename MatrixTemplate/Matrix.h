ceec1a0d6d1d21a351f839beb571bec7286f63a0
msp196811 <msp196811@gmail.com>
2019-11-06 09:20:57 +0300

    Matrix.h

--- MatrixTemplate/Matrix.h
+++ MatrixTemplate/Matrix.h
@@ -9,7 +9,60 @@ private:
 	int col;
 	T** matrix;
 public:
-	Matrix(int rowNum, int colNum) {
+	Matrix(int rowNum, int colNum);
+	~Matrix();
+	Matrix(const Matrix& temp);
+
+
+	void resize(int newRow, int newCol);
+	int setElem(int i_Row, int i_Col, double value);
+	template<typename T>
+	Matrix<T>& operator = (const Matrix<T>& b) {
+		if (getRow() != b.row || getCol() != b.col) {
+			this->resize(b.row, b.col);
+
+		}
+		**matrix = **b.matrix;
+		return (*this);
+
+	}
+	int getRow();
+	int getCol();
+
+
+	template<class T>
+	friend Matrix<T> operator+(const Matrix<T>& a, const Matrix<T>& b);
+
+	template<typename T>
+	friend Matrix<T> operator-(const Matrix<T>& a, const Matrix<T>& b);
+
+	template<typename T>
+	friend Matrix<T> operator*(const Matrix<T>& a, const Matrix<T>& b);
+
+	template<typename T>
+	friend Matrix<T> operator*(const Matrix<T>& a, double b);
+
+	template<typename T>
+	friend Matrix<T> operator*(double b, const Matrix<T>& a) {
+		return a * b;
+	}
+	template<typename T>
+	friend Matrix<T> operator/(const Matrix<T>& a, double b);
+
+	template<typename T>
+	friend istream& operator >> (istream& in, Matrix<T>& ob);
+
+	template<typename T>
+	friend ostream& operator << (ostream& out, Matrix<T>& ob);
+
+	};
+
+
+
+
+	
+	template<class T>
+	Matrix<T>::Matrix<T>(int rowNum, int colNum) {
 		matrix = new T * [rowNum];
 		for (int i = 0; i < rowNum; ++i) {
 			matrix[i] = new T[colNum];
@@ -18,13 +71,17 @@ public:
 		row = rowNum;
 		col = colNum;
 	}
-	~Matrix()
-	{
+
+	template<class T>
+	Matrix<T>::~Matrix<T>()
+	{	
 		for (int i = 0; i < row; ++i)
 			delete[] matrix[i];
 		delete[] matrix;
 	}
-	Matrix(const Matrix& temp) {
+
+	template<class T>
+	Matrix<T>::Matrix<T>(const Matrix<T>& temp) {
 		row = temp.row;
 		col = temp.col;
 
@@ -45,7 +102,10 @@ public:
 
 
 	}
-	void resize(int newRow, int newCol) {
+
+
+	template<class T>
+	void Matrix<T>::resize(int newRow, int newCol) {
 
 		T** temp;
 		temp = new T * [newRow];
@@ -64,29 +124,32 @@ public:
 		col = newCol;
 
 	}
-	template<typename T>
-	Matrix<T>& operator = (const Matrix<T>& b) {
-		if (getRow() != b.row || getCol() != b.col) {
-			this->resize(b.row, b.col);
 
-		}
-		**matrix = **b.matrix;
-		return (*this);
 
+	template<class T>
+	int Matrix<T>::setElem(int i_Row, int i_Col, double value) {
+		if (i_Row >= row || i_Col >= col) { exit(0); }
+		this->matrix[i_Row][i_Col] = value;
+		return 0;
 	}
-	int getRow() {
+
+	template<class T>
+	int Matrix<T>::getRow() {
 		return this->row;
 	}
-	int getCol() {
+
+	template<class T>
+	int Matrix<T>::getCol() {
 		return this->col;
 	}
-	template<typename T>
-	friend Matrix<T> operator+(const Matrix<T>& a, const Matrix<T>& b) {
+
+	template<class T>
+	Matrix<T> operator+(const Matrix<T>& a, const Matrix<T>& b) {
 		if (a.col != b.col || a.row != a.row) {
 			cout << "Матрицы разного размера" << endl;
 			exit(1);
 		}
-		Matrix temp(a.row, a.col);
+		Matrix<T> temp(a.row, a.col);
 		for (int i = 0; i < a.row; i++) {
 			for (int j = 0; j < a.col; j++) {
 				temp.setElem(i, j, (a.matrix[i][j] + b.matrix[i][j]));
@@ -94,13 +157,14 @@ public:
 		}
 		return temp;
 	}
+
 	template<typename T>
-	friend Matrix<T> operator-(const Matrix<T>& a, const Matrix<T>& b) {
+	Matrix<T> operator-(const Matrix<T>& a, const Matrix<T>& b) {
 		if (a.col != b.col || a.row != b.row) {
 			cout << "Матрицы разного размера" << endl;
 			exit(1);
 		}
-		Matrix temp(a.row, a.col);
+		Matrix<T> temp(a.row, a.col);
 		for (int i = 0; i < a.row; i++) {
 			for (int j = 0; j < a.col; j++) {
 				temp.setElem(i, j, (a.matrix[i][j] - b.matrix[i][j]));
@@ -108,13 +172,14 @@ public:
 		}
 		return temp;
 	}
+
 	template<typename T>
-	friend Matrix<T> operator*(const Matrix<T>& a, const Matrix<T>& b) {
+	Matrix<T> operator*(const Matrix<T>& a, const Matrix<T>& b) {
 		if (a.col != b.row) {
 			cout << "Умножение невозможно" << endl;
 			exit(1);
 		}
-		Matrix temp(a.row, b.col);
+		Matrix<T> temp(a.row, b.col);
 		for (int i = 0; i < a.row; i++) {
 			for (int j = 0; j < b.col; j++) {
 				temp.matrix[i][j] = 0;
@@ -124,9 +189,10 @@ public:
 		}
 		return temp;
 	}
+
 	template<typename T>
-	friend Matrix<T> operator*(const Matrix<T>& a, double b) {
-		Matrix temp(a.row, a.col);
+	Matrix<T> operator*(const Matrix<T>& a, double b) {
+		Matrix<T> temp(a.row, a.col);
 		for (int i = 0; i < a.row; i++) {
 			for (int j = 0; j < a.col; j++) {
 				temp.matrix[i][j] = a.matrix[i][j] * b;
@@ -134,16 +200,16 @@ public:
 		}
 		return temp;
 	}
+
+
 	template<typename T>
-	friend Matrix<T> operator*(double b, const Matrix<T>& a) {
-		return a * b;
-	}
-	template<typename T>
-	friend Matrix<T> operator/(const Matrix<T>& a, double b) {
+	Matrix<T> operator/(const Matrix<T>& a, double b) {
 		return a * (1 / b);
 	}
+
+
 	template<typename T>
-	friend istream& operator >> (istream& in, Matrix<T>& ob)
+	istream& operator >> (istream& in, Matrix<T>& ob)
 	{
 		int i, j;
 		for (i = 0; i < ob.row; ++i)
@@ -154,8 +220,9 @@ public:
 			}
 		return in;
 	}
+
 	template<typename T>
-	friend ostream& operator << (ostream& out, Matrix<T>& ob)
+	ostream& operator << (ostream& out, Matrix<T>& ob)
 	{
 		int i, j;
 
@@ -169,8 +236,3 @@ public:
 		out << endl;
 		return out;
 	}
-
-
-	};
-
-
--- MatrixTemplate/main.cpp
+++ MatrixTemplate/main.cpp
@@ -9,7 +9,7 @@ int main()
 	Matrix<double> m_2(m);
 	cout << m;
 	cout << m_2;
-	Matrix<double> m_3 = m * m_2;
+	Matrix<double> m_3 = m + m_2;
 	cout << m_3;
 	Matrix<double> m_4 = m_3 / 2;
 	cout << m_4;
